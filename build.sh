effe97e3540907215e449ea89b6dc14aade8b313
turusore08 <rhamabiasa@gmail.com>
2025-08-14 12:12:07 +0700

    Resolve merge conflict

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
@@ -6,18 +6,3 @@ set -o errexit
 pip install -r requirements.txt
 
 flask db upgrade
-=======
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# --- PERUBAHAN KRITIS UNTUK DEBUGGING ---
-# Tampilkan isi requirements.txt untuk verifikasi
-echo "--- Menampilkan isi requirements.txt ---"
-cat requirements.txt
-echo "------------------------------------"
-
-pip install -r requirements.txt
-
-flask db upgrade
->>>>>>> 842ccc201207f8e5287f310d4f6704aab83210cc
